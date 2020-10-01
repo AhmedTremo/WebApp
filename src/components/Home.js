e41c77fd42e9fbd829c903a7dab671166c8af87e
reemalik <47259821+reemalik@users.noreply.github.com>
2020-10-01 17:03:16 +0200

    added home page elements, CSS file, and modified results

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,8 +13,8 @@ return (
 {/* <h4>Hey there,</h4> */}
 <h1>The Industrial Digitisation 4.0</h1>
 <h3>A project by GUC students </h3>
-<p>The 18th Century was the spark that ignited the first indsutrial revolution, where factories 
-    and automobiles were powered by steam. The second indsutrial revolution graced the world mid the 19th century. 
+<p>The 18th Century was the spark that ignited the first industrial revolution, where factories 
+    and automobiles were powered by steam. The second industrial revolution graced the world mid the 19th century. 
     It was the beginning of a global mass production, and the third industrial revolution followed closely mid 
     the 20th century, right after the second world war. It was the era of the emergence of computer driven
     systems. The 21st century has witnessed, and still is witnessing, a remarkable and a rapid progression regarding
